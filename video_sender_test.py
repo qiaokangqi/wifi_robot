e865385322225f56872028adf92a740d6f75a0f8
kangChi <785659032@qq.com>
2017-12-16 23:36:02 +0800

    simplify
    create a simplify video_sender

--- video_sender_test.py
+++ video_sender_test.py
@@ -8,121 +8,6 @@ import cv2
 import time
 import os
 import numpy
-
-class webCamera: 
-    def __init__(self, resolution = (640, 480), host = ("", 7999)):      
-        self.resolution = resolution;      
-        self.host = host;      
-        self.setSocket(self.host);    
-        self.img_quality = 95
-        self.flag=0
-        
-    def setImageResolution(self, resolution):      
-        self.resolution = resolution;  
-        
-    def setHost(self, host):      
-        self.host = host;  
-        
-    def setSocket(self, host):      
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);                  
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1);              
-        self.socket.bind(self.host);      
-        self.socket.listen(5);      
-        print "Server running on port:%d" % host[1]
-        
-    def recv_config(self,client):   
-        client.settimeout(2)
-        info = struct.unpack("lhh",client.recv(8));   
-        if len(info) == 0:
-            return 0
-        if info[0]>911:        
-            print info[0]   
-            self.img_quality=int(info[0])-911              
-            self.resolution=list(self.resolution)        
-            self.resolution[0]=info[1]        
-            self.resolution[1]=info[2]        
-            self.resolution=tuple(self.resolution)                
-            return 1    
-        else :        
-            return 0
-            
-    def _processConnection(self, client,addr):     
-        if(self.recv_config(client)==0):        
-            return    
-        camera = cv2.VideoCapture(0)    
-        encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),self.img_quality]  
-        '''           
-        f = open("video_info.txt", 'a+')    
-        print "Got connection from %s:%d" % (addr[0], addr[1])
-        print "Pixel:%d * %d"%(self.resolution[0],self.resolution[1])
-        print "Camera open success" 
-        print "Connecting start time:%s"%time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))
-        f.close()    
-        '''
-        while(1):        
-            #time.sleep(0.1)        
-            (grabbed, img) = camera.read()  
-            '''
-            cv2.imshow('test',self.img)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-            '''
-            try:
-                len(img)
-            except:
-                camera.release()
-                print 'Get image failed! maybe camera is in use!'
-                return
-                
-            '''
-            if img == None:
-                camera.release()
-                print 'Get image failed'
-                return
-            '''
-            
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            
-            img  = cv2.resize(img,self.resolution)   
-            '''
-            if self.flag==0:
-                self.last_image=img.copy()
-                self.flag=1
-            dimage=cv2.subtract(img, self.last_image)
-            '''
-            #img = cv2.GaussianBlur(img,(3,3),0)  
-            #img = cv2.Canny(img, 50, 150)  
-            
-            encode_result, img_encoded = cv2.imencode('.jpg',img, encode_param)     
-            img_code = numpy.array(img_encoded)        
-            imgdata  = img_code.tostring()   
-
-            try:                    
-                client.send(struct.pack("lhh",len(imgdata),
-                        self.resolution[0],self.resolution[1])+imgdata); #发送图片信息(图片长度,分辨率,图片内容)                
-            except:    
-                
-                #f = open("video_info.txt", 'a+')            
-                print "%s:%d disconnected!" % (addr[0], addr[1])         
-                print "Connecting end time:%s"%time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))
-                print "****************************************"
-                camera.release()
-                #cv2.destroyAllWindows()             
-                #f.close()        
-                
-                return
-            #self.last_image=img.copy()
-            
-    def run(self):  
-        while True:        
-            client,addr = self.socket.accept()
-            print client,addr
-            clientThread = threading.Thread(target = self._processConnection, args = (client, addr, ))  #有客户端连接时产生新的线程进行处理                      
-            clientThread.start()
-def main():      
-    cam = webCamera()       
-    cam.run()
-
     
 def process_image(client, addr):
     img_quality=0
@@ -187,8 +72,7 @@ def process_image(client, addr):
             return
     
 
-if __name__ == "__main__":      
-    #main()
+if __name__ == "__main__":  
     host = ("", 7999)
     socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM);                  
     socket_obj.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1);              
